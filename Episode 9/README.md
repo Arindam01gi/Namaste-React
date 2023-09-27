fb85bb5ca809f333c5b78a72ce2cb4d7d8c80e80
Arindam Maiti <arindammaiti2018@gmail.com>
2023-09-27 10:58:29 +0530

    episode 8

---
+++ Episode 9/README.md
@@ -0,0 +1,19 @@
+/**
+ * Header
+ *  -Logo
+ *  -Nav Items
+ * Body
+ *  -Search
+ *  -RestaurantContainer
+ *    -Restaurant Card
+ *      - Image
+ *      - Name of Restaurant,star Ratings, Cuisine,time
+ *      - Name of Food
+ *      - Rating
+ * Footer
+ *  -Copyright
+ *  -Links
+ *  -Address
+ *  -Contact
+ *
+ */
---
+++ Episode 9/src/components/RestaurantMenu.js
@@ -0,0 +1,55 @@
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
+import { useParams } from "react-router-dom";
+import { RESTAURANT_ITEM_API } from "../utils/constants";
+
+const RestaurantMenu = () => {
+  useEffect(() => {
+    fetchMenu();
+  }, []);
+
+  const [resInfo, setResInfo] = useState(null);
+
+  const { resId } = useParams();
+  
+  const fetchMenu = async () => {
+    
+    const data = await fetch(RESTAURANT_ITEM_API + resId)
+    // const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.6232102&lng=88.4106973&restaurantId=87313");
+    const json = await data.json();
+    console.log(json);
+    setResInfo(json.data);
+  };
+
+  if (resInfo === null) {
+    return <Shimmer />;
+  }
+
+  const { name, cuisines, costForTwoMessage } =
+    resInfo?.cards[0]?.card?.card?.info;
+
+  const { itemCards } =
+    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
+
+
+  return (
+    <div className="menu">
+      <h1>{name}</h1>
+      <p>{cuisines.join(" ,")}</p>
+      <p>{costForTwoMessage}</p>
+      <h2>Menu</h2>
+      <ul>
+        {itemCards.map((item) => (
+          <li key={item?.card?.info?.id}>
+            {item?.card?.info?.name}-{" "}
+            {item?.card?.info?.defaultPrice / 100 ||
+              item?.card?.info?.price / 100}
+            <p>{item?.card?.info?.description}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RestaurantMenu;
